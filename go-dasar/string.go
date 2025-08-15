d1e53a8faeb030b3f6711cab1456627aed4547b1
fazrisuhada007 <fazrisuhada3@gmail.com>
2025-08-15 22:16:24 +0700

    Go-Dasar: Tipe Data String

---
+++ go-dasar/string.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+/*
+	function untuk string
+	len(“string”) => Menghitung jumlah karakter di String
+	“string”[number] => Mengambil karakter pada posisi yang ditentukan
+*/
+
+func main() {
+	fmt.Println("Fazri Suhada")
+	fmt.Println(len("Fazri Suhada"))
+	fmt.Println("Fazri Suhada"[1])
+}
