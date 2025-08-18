646bd7bc1032badec5d947e33b11f9144f3f990d
fazrisuhada007 <fazrisuhada3@gmail.com>
2025-08-18 20:35:45 +0700

    Go-Dasar: Operasi Perbandingan

---
+++ go-dasar/operasi_perbandingan.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	// minuman1 := "kopi susu"
+	// minuman2 := "kopi susu"
+	// minuman3 := "Kopi Susu"
+
+	a := "a"
+	b := "b"
+
+	// bool := minuman1 != minuman3
+	bool := a > b
+	fmt.Println(bool)
+}
