209ec644762d05693223b9c27d456df5de83b37f
fazrisuhada007 <fazrisuhada3@gmail.com>
2025-08-20 18:25:24 +0700

    Go-Dasar: Variadic Funciton

--- go-dasar/variadic_function.go
+++ go-dasar/variadic_function.go
@@ -16,4 +16,12 @@ func main() {
 	fmt.Println(sumAll(5, 5, 5))
 	fmt.Println(sumAll(5, 5, 5, 5))
 	fmt.Println(sumAll(5, 5, 5, 5, 5))
+
+	// kode function slice parameter
+	total2 := sumAll(10, 10, 10, 10)
+	fmt.Println(total2)
+
+	numbers2 := []int{10, 10, 10, 10}
+	total2 = sumAll(numbers2...)
+	fmt.Println()
 }
