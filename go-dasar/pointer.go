12380103ff153c60632fe91a4352e229ddb2fe06
fazrisuhada007 <fazrisuhada3@gmail.com>
2025-08-21 18:43:05 +0700

    Go-Dasar: Pointer

--- go-dasar/pointer.go
+++ go-dasar/pointer.go
@@ -13,6 +13,11 @@ func main() {
 	var address2 = &address1 //pointer gunakan &
 
 	address2.City = "Payakumbuh"
+	fmt.Println(address1)
+	fmt.Println(address2)
+
+	// address2 = &Address{"Jakarta","DKI Jakarta","Indonesia"}
+	*address2 = Address{"Jakarta", "DKI Jakarta", "Indonesia"}
 
 	fmt.Println(address1)
 	fmt.Println(address2)
