35b2e10306129243191968b4bc7661017c0ff810
fazrisuhada007 <fazrisuhada3@gmail.com>
2025-08-21 20:02:44 +0700

    Go-Dasar: Pointer New

--- go-dasar/pointer.go
+++ go-dasar/pointer.go
@@ -6,11 +6,16 @@ type Address struct {
 	City, Province, Country string
 }
 
+type Person struct {
+	Name string
+	Age  int
+}
+
 func main() {
 
 	var address1 = Address{"Bukittinggi", "Sumatra Barat", "Indonesia"}
 
-	var address2 = &address1 //pointer gunakan &
+	var address2 = &address1 //pointer gunakan & atau new
 
 	address2.City = "Payakumbuh"
 	fmt.Println(address1)
@@ -21,5 +26,12 @@ func main() {
 
 	fmt.Println(address1)
 	fmt.Println(address2)
+
+	person1 := Person{"Fazri", 31}
+	person2 := new(Person)
+	person2.Name = "Suhada"
+	person2.Age = 10
+	fmt.Println(person1)
+	fmt.Println(person2)
 
 }
