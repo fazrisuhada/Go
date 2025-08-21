f726187f3b4bceba53f1afe6d28ffb52bf05f67d
fazrisuhada007 <fazrisuhada3@gmail.com>
2025-08-21 17:31:34 +0700

    Go-Dasar: Pointer

---
+++ go-dasar/pointer.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+type Address struct {
+	City, Province, Country string
+}
+
+func main() {
+
+	var address1 = Address{"Bukittinggi", "Sumatra Barat", "Indonesia"}
+
+	var address2 = &address1 //pointer gunakan &
+
+	address2.City = "Payakumbuh"
+
+	fmt.Println(address1)
+	fmt.Println(address2)
+
+}
