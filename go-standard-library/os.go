ecb3e8201c679366a6296c17ddfcda75c23ab826
fazrisuhada007 <fazrisuhada3@gmail.com>
2025-08-22 21:35:21 +0700

    Go-Standard-Library: os

---
+++ go-standard-library/os.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	args := os.Args
+
+	for _, arg := range args {
+		fmt.Println(arg)
+	}
+
+	hostName, err := os.Hostname()
+	if err == nil {
+		fmt.Println(hostName)
+	} else {
+		fmt.Println(err.Error())
+	}
+}
