8b379b93ef3879b37a07867683d718aaf50ebcc0
Fazri Suhada <fazrisuhada56@gmail.com>
2025-09-16 15:06:11 +0700

    Go Module : Menambah Dependency

--- go-module/app-say-hello/go.mod
+++ go-module/app-say-hello/go.mod
@@ -1,3 +1,5 @@
 module app-say-hello
 
 go 1.25.0
+
+require github.com/fazrisuhada/Go/go-module/go-say-hello v0.0.0-20250916075517-ada2dd104b3f // indirect
---
+++ go-module/app-say-hello/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	gosayhello "github.com/fazrisuhada/Go/go-module/go-say-hello"
+)
+
+func main() {
+	fmt.Println(gosayhello.SayHello())
+}
