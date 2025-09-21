fea922ec7b59f6366c019a3c09ff3eb5245249ef
Fazri Suhada <fazrisuhada56@gmail.com>
2025-09-22 06:12:01 +0700

    go unit test : menggagalkan unit test

--- go-unit-test/helper/hello_world_test.go
+++ go-unit-test/helper/hello_world_test.go
@@ -1,11 +1,17 @@
 package helper
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestHelloWorld(t *testing.T) {
 	result := HelloWorld("Fazri")
 
 	if result != "Hello Fazri" {
-		panic("Result is not 'Hello Fazri'")
+		t.Error("Result must be 'Hello Fazri'")
+		// t.Fatal("Result must be 'Hello Fazri'")
 	}
+
+	fmt.Println("Test Hello World Done")
 }
