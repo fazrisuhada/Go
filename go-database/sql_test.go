39cedb748f0f0fcd524a5ee696bb25057f1df180
Fazri Suhada <fazrisuhada56@gmail.com>
2025-12-29 04:05:07 +0700

    golang database : query sql

--- go-database/sql_test.go
+++ go-database/sql_test.go
@@ -22,3 +22,32 @@ func TestExcSQL(t *testing.T) {
 
 	fmt.Println("Success insert new customer")
 }
+
+func TestQuerySQL(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	script := "SELECT id,name from customers"
+
+	rows, err := db.QueryContext(ctx, script)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var id, name string
+		err = rows.Scan(&id, &name)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("id:", id)
+		fmt.Println("name:", name)
+	}
+
+}
