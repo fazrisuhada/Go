55e524444f70a1a938fcecb609a852ff93061849
Fazri Suhada <fazrisuhada56@gmail.com>
2025-12-30 15:56:12 +0700

    golang database : tipe data colomn

--- go-database/sql_test.go
+++ go-database/sql_test.go
@@ -2,8 +2,10 @@ package godatabase
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
 	"testing"
+	"time"
 )
 
 func TestExcSQL(t *testing.T) {
@@ -29,7 +31,7 @@ func TestQuerySQL(t *testing.T) {
 
 	ctx := context.Background()
 
-	script := "SELECT id,name from customers"
+	script := "SELECT id, name, email, balance, rating, birth_date, married, created_at from customers"
 
 	rows, err := db.QueryContext(ctx, script)
 
@@ -41,13 +43,31 @@ func TestQuerySQL(t *testing.T) {
 
 	for rows.Next() {
 		var id, name string
-		err = rows.Scan(&id, &name)
+		var email sql.NullString
+		var balance int32
+		var rating float64
+		var birthDate sql.NullTime
+		var createdAt time.Time
+		var married bool
+
+		err = rows.Scan(&id, &name, &email, &balance, &rating, &birthDate, &married, &createdAt)
 		if err != nil {
 			panic(err)
 		}
 
+		fmt.Println("================================")
 		fmt.Println("id:", id)
 		fmt.Println("name:", name)
+		if email.Valid {
+			fmt.Println("email:", email.String)
+		}
+		fmt.Println("balance:", balance)
+		fmt.Println("rating:", rating)
+		if birthDate.Valid {
+			fmt.Println("birth_date:", birthDate.Time)
+		}
+		fmt.Println("married:", married)
+		fmt.Println("created_at:", createdAt)
 	}
 
 }
